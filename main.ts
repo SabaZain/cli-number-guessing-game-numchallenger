06bbef533a7c1ab35e45a27ff0eb66ffe88296e8
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-03-26 13:25:46 +0500

    Adding CLI Number Guessing Game Project

---
+++ main.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+// CLI Number Guessing Game
+
+import inquirer from "inquirer";
+
+console.log("Welcome to CodeWithSaba-CLI Number Guessing Game-NumChallenger");
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess number(Number Limit from 1 to 6):",
+    },
+]);
+if(answer.userGuessedNumber === randomNumber){
+    console.log("Congratulation! You guess a correct number");
+} else {
+    console.log("Sorry, You guess a wrong number");
+}
